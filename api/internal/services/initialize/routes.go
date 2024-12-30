340274eaf3f692d0bf9eb0b39fd9eb7b21a0169f
Masoud <mmtaee64@gmail.com>
2024-12-30 22:30:57 +0330

    update schema and migrate some routes

--- api/internal/services/initialize/routes.go
+++ api/internal/services/initialize/routes.go
@@ -1,44 +1,8 @@
 package initialize
 
 import (
-	"api/pkg/config"
-	"errors"
 	"github.com/labstack/echo/v4"
-	"log"
-	"net/http"
-	"os"
 )
-
-func IntiRoutePermissionMiddleware() echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			file := config.GetApp().InitSecretFile
-			_, err := os.Stat(file)
-			if err != nil {
-				if !errors.Is(err, os.ErrNotExist) {
-					log.Println(err)
-				}
-				log.Println(err)
-				return echo.ErrForbidden
-			}
-			secretKey := c.QueryParam("init_secret")
-			if secretKey == "" {
-				log.Println("secret key is empty")
-				return echo.ErrForbidden
-			}
-			content, err := os.ReadFile(config.GetApp().InitSecretFile)
-			if err != nil {
-				log.Println(err)
-				return c.JSON(http.StatusNotFound, nil)
-			}
-			if secretKey != string(content) {
-				log.Println("invalid secret key")
-				return echo.ErrForbidden
-			}
-			return next(c)
-		}
-	}
-}
 
 func Routes(e *echo.Group) {
 	controller := New()
@@ -46,5 +10,4 @@ func Routes(e *echo.Group) {
 	group.GET("/check", controller.CheckSecretKey)
 	group.POST("/admin", controller.CreateSuperUser)
 	group.POST("/config", controller.PanelConfig)
-
 }
