b9b9835abe3b3b6a4a78e6462c3fd8a7493f06a2
Masoud <mmtaee64@gmail.com>
2025-01-03 20:24:11 +0330

    start ocserv user api

--- api/internal/services/oc_user/controller.go
+++ api/internal/services/oc_user/controller.go
@@ -1,1 +1,60 @@
-package oc_user
+package ocUser
+
+import (
+	"api/internal/repository"
+	"api/pkg/database"
+	"api/pkg/validator"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+type Controller struct {
+	db             *gorm.DB
+	validator      validator.CustomValidatorInterface
+	ocservUserRepo repository.OcservUserRepositoryInterface
+}
+
+func New() *Controller {
+	return &Controller{
+		db:             database.Connection(),
+		validator:      validator.NewCustomValidator(),
+		ocservUserRepo: repository.NewOcservUserRepository(),
+	}
+}
+
+func (ctrl *Controller) Users(c echo.Context) error {
+	return nil
+}
+
+func (ctrl *Controller) User(c echo.Context) error {
+	return nil
+}
+
+func (ctrl *Controller) Create(c echo.Context) error {
+	return nil
+}
+
+func (ctrl *Controller) Update(c echo.Context) error {
+	return nil
+}
+
+func (ctrl *Controller) Lock(c echo.Context) error {
+	// TODO: get lock true or false from body
+	return nil
+}
+
+func (ctrl *Controller) Disconnect(c echo.Context) error {
+	return nil
+}
+
+func (ctrl *Controller) Delete(c echo.Context) error {
+	return nil
+}
+
+func (ctrl *Controller) Statistics(c echo.Context) error {
+	return nil
+}
+
+func (ctrl *Controller) Activity(c echo.Context) error {
+	return nil
+}
--- api/internal/services/oc_user/routes.go
+++ api/internal/services/oc_user/routes.go
@@ -1,1 +1,17 @@
-package oc_user
+package ocUser
+
+import "github.com/labstack/echo/v4"
+
+func Routes(e *echo.Group) {
+	controller := New()
+	group := e.Group("/ocserv/users")
+	group.GET("", controller.Users)
+	group.POST("", controller.Create)
+	group.GET("/:id", controller.User)
+	group.PATCH("/:id", controller.Update)
+	group.POST("/:id/lock", controller.Lock)
+	group.POST("/:id/disconnect", controller.Disconnect)
+	group.DELETE("/:id", controller.Delete)
+	group.GET("/:id/activity", controller.Activity)
+	group.GET("/:id/statistics", controller.Statistics)
+}
