e540b32f63582965f6d2f37bf5cc3429d9628e74
Masoud <mmtaee64@gmail.com>
2025-01-08 11:34:07 +0330

    linter on route

--- api/internal/services/panel/routes.go
+++ api/internal/services/panel/routes.go
@@ -8,11 +8,13 @@ import (
 func Routes(e *echo.Group) {
 	controller := New()
 	e.GET("/panel/config/init", controller.GetPanelInitConfig)
+
 	panelGroup := e.Group(
 		"/panel",
 		middlewares.IsAuthenticatedMiddleware(),
 		middlewares.IsAdminPermissionMiddleware(),
 	)
+
 	panelGroup.POST("/config", controller.CreatePanelConfig)
 	panelGroup.PATCH("/config", controller.UpdatePanelConfig)
 	panelGroup.GET("/config", controller.GetPanelConfig)
