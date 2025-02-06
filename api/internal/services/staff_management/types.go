3a313ed1490b9a464fe33a4a43b37ecc27e8bb8c
Masoud <mmtaee64@gmail.com>
2025-02-06 21:05:56 +0330

    fix some bugs and update models request and response

--- api/internal/services/staff_management/types.go
+++ api/internal/services/staff_management/types.go
@@ -16,20 +16,22 @@ type CreateStaffRequest struct {
 		Password string `json:"password" validate:"required,min=2,max=16" example:"doe123456"`
 	} `json:"user" validator:"required"`
 	Permission struct {
-		OcUser    bool `json:"oc_user" validator:"required"`
-		OcGroup   bool `json:"oc_group" validator:"required"`
-		Statistic bool `json:"statistic" validator:"required"`
-		Occtl     bool `json:"occtl" validator:"required"`
-		System    bool `json:"system" validator:"required"`
+		OcUser       bool `json:"oc_user" validator:"required"`
+		OcGroup      bool `json:"oc_group" validator:"required"`
+		Statistic    bool `json:"statistic" validator:"required"`
+		Occtl        bool `json:"occtl" validator:"required"`
+		System       bool `json:"system" validator:"required"`
+		SeeServerLog bool `json:"see_server_log" validator:"required"`
 	} `json:"permission" validator:"required"`
 }
 
 type UpdateStaffPermissionRequest struct {
-	OcUser    *bool `json:"oc_user" validator:"required"`
-	OcGroup   *bool `json:"oc_group" validator:"required"`
-	Statistic *bool `json:"statistic" validator:"required"`
-	Occtl     *bool `json:"occtl" validator:"required"`
-	System    *bool `json:"system" validator:"required"`
+	OcUser       *bool `json:"oc_user" validator:"required"`
+	OcGroup      *bool `json:"oc_group" validator:"required"`
+	Statistic    *bool `json:"statistic" validator:"required"`
+	Occtl        *bool `json:"occtl" validator:"required"`
+	System       *bool `json:"system" validator:"required"`
+	SeeServerLog *bool `json:"see_server_log" validator:"required"`
 }
 
 type UpdateStaffPasswordRequest struct {
